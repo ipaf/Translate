f36e92c3bf4e2e139cc7a0d4e92aeb6d833e7546
Ivan Pavlov <pafes103@rambler.ru>
2017-04-06 07:28:52 +0300

    [fix] translate view interface.

--- app/src/main/java/ru/pascalman/translate/view/TranslateView.java
+++ app/src/main/java/ru/pascalman/translate/view/TranslateView.java
@@ -1,14 +1,10 @@
 package ru.pascalman.translate.view;
 
-import java.util.List;
-
-import ru.pascalman.translate.presenter.SynItem;
-
-public interface TranslateView extends ListView<SynItem>
+public interface TranslateView extends ListView<String>
 {
 
     void setDefaultLanguages(String translateFrom, String translateTo);
     String getTranslateText();
-    void showTranslatedText(String translatedText, String pos, List<String> syns);
+    void showTranslatedText(String translatedText, String pos);
 
 }
